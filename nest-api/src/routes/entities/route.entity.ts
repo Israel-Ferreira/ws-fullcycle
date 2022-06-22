2bef6dcf0baf336603b1864444fd4c390d9b7972
israel.souza <israel.souza@summit-bra.com>
2022-06-22 13:40:01 -0300

    env

--- nest-api/src/routes/entities/route.entity.ts
+++ nest-api/src/routes/entities/route.entity.ts
@@ -1,1 +1,32 @@
-export class Route {}
+import {Prop, raw, Schema, SchemaFactory} from '@nestjs/mongoose'
+import { Document } from 'mongoose';
+import { Position } from './position';
+
+
+export type RouteDocument = Route & Document
+
+@Schema()
+export class Route {
+
+    @Prop()
+    title: string;
+
+    @Prop(
+        raw({
+            lat: {type: Number},
+            lng: {type: Number}
+        })
+    )
+    startPosition: Position
+
+    @Prop(
+        raw({
+            lat: {type: Number},
+            lng: {type: Number}
+        })
+    )
+    endPosition: Position
+}
+
+
+export const RouteSchema = SchemaFactory.createForClass(Route)
