615166a197d0941628d9cc9965649e7153a72ad9
israel.souza <israel.souza@summit-bra.com>
2022-06-23 17:50:22 -0300

    Add google maps

--- react-frontend/src/components/Mapping.tsx
+++ react-frontend/src/components/Mapping.tsx
@@ -1,7 +1,9 @@
-import React, { FormEvent, useCallback, useEffect, useState } from 'react';
+import React, { FormEvent, useCallback, useEffect, useRef, useState } from 'react';
 
 import {Button, Grid, MenuItem, Select} from '@mui/material'
 import { Route } from '../utils/route';
+import { Loader } from 'google-maps';
+import { getCurrentPosition } from '../utils/geolocation';
 
 
 const API_URL = process.env.REACT_APP_API_URL
@@ -11,11 +13,32 @@ function Mapping() {
     const [routes, setRoutes] = useState<Route[]>([])
     const [routeIdSelected, setRouteIdSelected] =  useState<string>('')
 
+    const googleMapsLoader =  new Loader(process.env.REACT_APP_GOOGLE_API_KEY)
+
+    const mapRef =  useRef<google.maps.Map>();
+
     useEffect(() => {
         fetch(`${API_URL}/routes`)
             .then(data => data.json())
             .then(data => setRoutes(data))
     }, [])
+
+
+    useEffect(() => {
+        (async () => {
+           const [, position] = await Promise.all([
+                googleMapsLoader.load(),
+                getCurrentPosition({enableHighAccuracy: true})
+            ])
+
+            const divMap =  document.getElementById("map") as HTMLElement
+            mapRef.current = new google.maps.Map(divMap, {
+                zoom: 15,
+                center: position
+            })
+            
+        })()
+    },[])
 
 
     const startRoute = useCallback((event: FormEvent) => {
@@ -25,11 +48,14 @@ function Mapping() {
 
     
     return (
-        <Grid container>
+        <Grid container style={{
+            width: '100%',
+            height: '100%'
+        }}>
             <Grid item xs={12} sm={3}>
                 <form onSubmit={startRoute}>
-                    <Select fullWidth value={routeIdSelected} onChange={(e) => setRouteIdSelected(e.target.value)}>
-                        <MenuItem value={""}>
+                    <Select fullWidth value={routeIdSelected} displayEmpty onChange={(e) => setRouteIdSelected(e.target.value)}>
+                        <MenuItem value="" selected>
                             <em>Selecione uma corrida</em>
                         </MenuItem>
                         {routes.map((rt, key) => (
@@ -41,7 +67,10 @@ function Mapping() {
                 </form>
             </Grid>
             <Grid item xs={12} sm={9}>
-                <div id="map"></div>
+                <div id="map" style={{
+                    width: '100%',
+                    height: '100%'
+                }}></div>
             </Grid>
         </Grid>
     );
---
+++ react-frontend/src/utils/geolocation.ts
@@ -0,0 +1,11 @@
+import { Position } from "./route";
+
+export function getCurrentPosition(options?: PositionOptions) : Promise<Position> {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            position => resolve({lat: position.coords.latitude, lng: position.coords.longitude}),
+            error => reject(error),
+            options
+        )
+    })
+}
