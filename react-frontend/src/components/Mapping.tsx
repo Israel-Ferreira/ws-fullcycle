31bb3e738b86b13187134ffcb6f8840fdbc99e72
israel.souza <israel.souza@summit-bra.com>
2022-06-23 13:32:00 -0300

    Adicionando a biblioteca material ui

---
+++ react-frontend/src/components/Mapping.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import {Grid} from '@mui/material'
+
+
+function Mapping() {
+    return (
+        <Grid container>
+            <Grid item>Formulário</Grid>
+            <Grid item>Mapa</Grid>
+        </Grid>
+    );
+}
+
+export default Mapping;
